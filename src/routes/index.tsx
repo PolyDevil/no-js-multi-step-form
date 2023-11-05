790ad1a2953b7b708abfaae9c755fd55500bd437
PolyDevil <polydevil@cocaine.ninja>
2023-11-05 16:48:18 +0300

    init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,152 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-
-import Counter from "~/components/starter/counter/counter";
-import Hero from "~/components/starter/hero/hero";
-import Infobox from "~/components/starter/infobox/infobox";
-import Starter from "~/components/starter/next-steps/next-steps";
+import "./styles.module.css";
 
 export default component$(() => {
   return (
     <>
-      <Hero />
-      <Starter />
+      <h1>Multistep form</h1>
 
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
+      <main>
+        <form>
+          <ul>
+            <li>
+              <article>
+                <h2 id="step1">What kind on Services do you need?</h2>
 
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
+                <fieldset>
+                  <legend>Select a service</legend>
+                  <label>
+                    <span>Option 1</span>
+                    <input type="radio" name="service" value="1" />
+                  </label>
+                  <label>
+                    <span>Option 2</span>
+                    <input type="radio" name="service" value="2" />
+                  </label>
+                  <label>
+                    <span>Option 3</span>
+                    <input type="radio" name="service" value="3" />
+                  </label>
+                </fieldset>
 
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
-
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
+                <a href="#step2">
+                  {"next"}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                    role="presentation"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                    ></path>
+                  </svg>
                 </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
+              </article>
+            </li>
+
+            <li>
+              <article>
+                <h2 id="step2">Your preferences</h2>
+
+                <fieldset>
+                  <legend>Select preferences</legend>
+                  <label>
+                    <span>Pref 1</span>
+                    <input type="checkbox" name="pref1" />
+                  </label>
+                  <label>
+                    <span>Pref 2</span>
+                    <input type="checkbox" name="pref2" />
+                  </label>
+                  <label>
+                    <span>Pref 3</span>
+                    <input type="checkbox" name="pref3" />
+                  </label>
+                </fieldset>
+
+                <a href="#step1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                    role="presentation"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M7.82843 10.9999H20V12.9999H7.82843L13.1924 18.3638L11.7782 19.778L4 11.9999L11.7782 4.22168L13.1924 5.63589L7.82843 10.9999Z"
+                    ></path>
+                  </svg>
+                  {"prev"}
                 </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
+                <a href="#step3">
+                  {"next"}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                    role="presentation"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                    ></path>
+                  </svg>
                 </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
+              </article>
+            </li>
+
+            <li>
+              <article>
+                <h2 id="step3">Contact information</h2>
+
+                <fieldset>
+                  <legend>Contact information</legend>
+                  <label>
+                    <span>Name</span>
+                    <input type="text" name="name" />
+                  </label>
+                  <label>
+                    <span>Email</span>
+                    <input type="email" name="email" />
+                  </label>
+                </fieldset>
+
+                <a href="#step2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                    role="presentation"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M7.82843 10.9999H20V12.9999H7.82843L13.1924 18.3638L11.7782 19.778L4 11.9999L11.7782 4.22168L13.1924 5.63589L7.82843 10.9999Z"
+                    ></path>
+                  </svg>
+                  {"prev"}
                 </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
-      </div>
+              </article>
+            </li>
+          </ul>
+        </form>
+      </main>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Multi-step form without js",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "madness",
     },
   ],
 };
